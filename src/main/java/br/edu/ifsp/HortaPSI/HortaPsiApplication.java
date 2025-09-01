0cfcafd005517f84cc4a7ad2d4abdce8d0e80b37
dreyrd <andreyrosadias8@gmail.com>
2025-09-01 19:55:33 -0300

    modelo do projeto

---
+++ src/main/java/br/edu/ifsp/HortaPSI/HortaPsiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.HortaPSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortaPsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortaPsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/HortaPSI/HortaPsiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.HortaPSI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortaPsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
